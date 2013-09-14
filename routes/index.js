14bce119c2d94556584d8e81add722d3d712745d
kunalkushwaha <kunal.kushwaha@gmail.com>
2013-09-14 15:59:19 +0530

    commiting index file

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hack13App - Twitter analytics to know more about technology usage' });
+};
+
+exports.search = function(req, res){
+  res.render('search', { title: 'Search Result' });
+};
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
 };
